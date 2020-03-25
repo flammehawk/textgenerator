86228239be526d884d27552368499ea502992469
flammehawk <57394581+flammehawk@users.noreply.github.com>
2020-03-25 09:56:51 +0100

    config(global): modified some of the configs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+    verbose: true,
     transform: { '^.+\\.ts?$': 'ts-jest' },
     testEnvironment: 'node',
     testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globals: {
+        'ts-jest': {
+            diagnostics: {
+                pathRegex: /\.(spec|test)\.ts$/,
+                warnOnly: true,
+            },
+        },
+    },
 }
